df4da2328cff5c02f10ea7fe0e225135aa8968e1
Salima <salimanaim18@gmail.com>
2025-10-13 16:38:44 +0100

    Deploy build to gh-pages

--- src/pages/projects-page.js
+++ src/pages/projects-page.js
@@ -268,13 +268,17 @@ export default function ProjectsPage() {
                 </>
               ) : selectedProject.mediaType === "video" && selectedProject.mediaUrl ? (
 <video
-  src={`/videos/${selectedProject.mediaUrl}`}
+  src={`videos/${selectedProject.mediaUrl}`}
   controls
   className="rounded-xl max-w-full max-h-[500px] w-auto h-auto"
 >
-  <source src={`/videos/${selectedProject.mediaUrl}`} type="video/mp4" />
+  <source src={`videos/${selectedProject.mediaUrl}`} type="video/mp4" />
   Your browser does not support the video tag.
 </video>
+
+
+
+
 
                  
               ) : (
